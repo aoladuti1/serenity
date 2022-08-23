b5faaddd366ec76a6fa07fd6901db3047c3cfe11
Antonio <ZJAC020@live.rhul.ac.uk>
2022-08-23 03:30:01 +0100

    added a function to check if a song is on db
    with additional refactoring

--- db.py
+++ db.py
@@ -1,11 +1,12 @@
 import sqlite3
+from typing import Sequence
 from config import *
 
-cursor = None
 
-def initDB():
-    global cursor
-    cursor = sqlite3.connect(DATABASE).cursor()
+def init():
+    global conn
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
     cursor.execute(
         """
         CREATE TABLE if not exists Songs (
@@ -15,8 +16,8 @@ def initDB():
             track text,
             trackNum integer,
             duration integer,
-            bitRate text,
-            samplingRate text,
+            bitRateInfo text,
+            samplingRateInfo text,
             channelCount integer,
             audioFormat text,
             art text,
@@ -24,9 +25,43 @@ def initDB():
             PRIMARY KEY(FQFN)
         )
         """
+        
     )
 
-    
+def songRegistered(FQFN):
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+        SELECT * FROM Songs 
+        WHERE FQFN = ?
+        LIMIT 1
+        """, [FQFN]
+        ) 
+    return cursor.fetchone() != None
 
+def updateSong(songData):
+    print("I AM NOT DEVELOPED YET !!!!```1`11`11111")
+
+
+def addSong(songData: Sequence):
+    """
+    Adds a song to the database.
+
+    Parameters:
+
+    songData: a string array of the correct format as returned
+    from addFolder.addFolderBox()
+        
+    """
+    if songData == None:
+        return
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+        INSERT INTO Songs VALUES (?,?,?,?,?,?,?,?,?,?,?,?)
+        """,
+        songData
+    )
+    conn.commit()
 
 
