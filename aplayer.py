4b261125c5722c6c3b7ecbe4ccc28ee0aaaf2769
Antonio <ZJAC020@live.rhul.ac.uk>
2022-08-25 12:19:18 +0100

    Aplayer: total class overhaul (now a singleton)
    This class is now a singleton.
    The underlying mplayer process is now a class variable (aplayer)
    It plays audio when instantiated.
    Added a startingVolume class variable.
    Added a setVolume() function to set current volume
    Added a terminate() class function.
    Fixed the issue of args not being passed to __init__

--- aplayer.py
+++ aplayer.py
@@ -1,5 +1,6 @@
 
 from asyncio.subprocess import PIPE
+from glob import glob
 import subprocess
 from sys import stdout
 import time
@@ -7,11 +8,21 @@ import threading
 
 class Aplayer:
     MPLAYER_DIR = 'C:\\Users\\anton\\Downloads\\mplayer-svn-38151-x86_64\\mplayer.exe'
-    
+    instances = 0
+    aplayer = None
+    startingVolume = 100
+
     # Will open a aplayer.exe
     # args is a list of additional arguments after -slave and before the fully qualified filename
-    def genProcess(self, fullFileName, args=[]):
-        argslist = [Aplayer.MPLAYER_DIR] + ['-slave', '-idle', '-pausing', '2'] + args + [fullFileName]
+    def genProcess(self, FQFN, args=[]):
+        # -pausing 2 means that no matter what command is passed through the PIPE, 
+        # the pause/play state stays the same        
+        argslist = (
+            [Aplayer.MPLAYER_DIR]
+          + ['-slave', '-idle', '-pausing', '2', '-volume', str(Aplayer.startingVolume)] 
+          + args 
+          + [FQFN]
+        )
         return subprocess.Popen(
             argslist, 
             stdout=subprocess.PIPE, 
@@ -21,39 +32,37 @@ class Aplayer:
             bufsize=1
             )   
 
-    def __init__(self, fullFileName=''):
-        if fullFileName == '':
-            self.aplayer = None
-        self.fullFileName = fullFileName
-        self.aplayer = self.genProcess(self.fullFileName)
-        self.aplayer.stdin.write("pause\n")
-        self.playing = False
-        self.ctext = ''
-        
-        
-
-    def clearctext(self):
-        self.ctext = ''
-
-    def signsOfLife(self) -> bool:
-        time.sleep(0.01)
-        ret = self.aplayer.poll() == None
-        if ret == False:
+    def __init__(self, FQFN='', args=[]):
+        if FQFN == '':
+            Aplayer.aplayer = None
             self.playing = False
-        return ret
+        Aplayer.instances += 1
+        if Aplayer.instances > 1:
+            Aplayer.aplayer.terminate()
+        Aplayer.aplayer = self.genProcess(FQFN, args)
+        self.FQFN = FQFN
+        self.playing = True
+        self.ctext = ''
+        threading.Thread(target=self.__playpriv).start()    
+
+    def terminate():
+        Aplayer.aplayer.terminate()
 
     def pwrite(self, text='') -> bool:
         sign = self.signsOfLife()
         if sign:
-            self.aplayer.stdin.write(text + "\n")
+            Aplayer.aplayer.stdin.write(text + "\n")
         return sign
         
 
     def __playpriv(self):
+        """
+        This function will ensure the playing audio keeps going normally.
+        It must be threaded. pauseplay() wraps it in a thread.
+        """
         while self.signsOfLife():
-            self.ctext += self.aplayer.stdout.readline()
-        #print(self.ctext) # WILL NOT print everything to screen with vscode debugger
-        #but it IS there in the ctext string
+            self.ctext += Aplayer.aplayer.stdout.readline()
+        Aplayer.terminate()
         return
              
 
@@ -64,16 +73,32 @@ class Aplayer:
         else:
             self.playing = True
             t1 = threading.Thread(target=self.__playpriv)
-            t1.start()
-        
+            t1.start()    
+
+    def clearctext(self):
+        self.ctext = ''
+
+    def signsOfLife(self) -> bool:
+        time.sleep(0.01)
+        ret = Aplayer.aplayer.poll() == None
+        if ret == False:
+            self.playing = False
+        return ret
   
     def quit(self):
         self.playing = False
         if not self.pwrite('quit'):
-            self.aplayer.terminate()
+            Aplayer.aplayer.terminate()
  
     def seek(self, plusMinusTime):
         self.pwrite("seek " + str(plusMinusTime))
 
-    
+    def setVolume(self, volume):
+        """
+        Sets the volume of the current audio (not the starting volume).
 
+        Parameters:
+        
+        volume: the new volume (between 1 and 100)
+        """
+        self.pwrite('volume ' + str(volume) + ' 1')
