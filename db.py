a1b46376480e11676f102d902d9ec2501bb1566a
Antonio <ZJAC020@live.rhul.ac.uk>
2022-08-25 03:15:00 +0100

    db: added deleteIf() function, mild neatening

--- db.py
+++ db.py
@@ -40,6 +40,43 @@ def songRegistered(FQFN: str):
         ) 
     return cursor.fetchone() != None
 
+def deleteIf(conditions: dict, negateConditions = False, conjunction: bool = True):
+    """
+    Deletes records from the database of Songs based off a dictionary of conditions.
+    e.g. deleteIf( {'artist' : 'Drake'} ) deletes all rows where Drake is the artist.
+    (equiv. sql: "WHERE artist = 'Drake'").
+    The NOT operator puts NOT in front of every boolean operator (AND / OR etc.)
+    like in the following example: if negateConditions == True and
+    conditions = {'artist' : 'Bryson Tiller', 'album' : 'T R A P S O U L'}
+    then the corresponding SQL would be: 
+    "WHERE NOT artist = 'Bryson Tiller' AND NOT album = 'T R A P S O U L'"
+
+    Parameters:
+    
+    conditions: the dictionary of conditions
+    negateConditions: if True then NOT is prepended to each boolean operator
+    conjunction: if True, the main boolean operator between conditions is "AND", otherwise it is "OR"
+
+    Returns:
+    True if something was deleted
+    """
+    body = ''
+    if negateConditions == True:
+        negationText = 'NOT '
+    else:
+        negationText = ''
+    spacedAndOr = ' and '
+    walk = -5
+    if conjunction == False:
+        spacedAndOr = ' or '
+        walk = -4
+    for key, val in conditions.items():
+        body += negationText + key + ' = :' + key + spacedAndOr
+    body = body[0:walk]
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM Songs WHERE " + body, conditions)
+    conn.commit()
+
 def deleteIfAbsent(FQFN: str) -> bool: 
     """
     Deletes music from the database that do not exist
@@ -55,11 +92,11 @@ def deleteIfAbsent(FQFN: str) -> bool:
 
     """
     cursor = conn.cursor()
-    if os.path.exists(FQFN):
+    if os.path.exists(FQFN) == True:
         return False
     cursor.execute(
         "DELETE FROM Songs " +
-        "WHERE FQFN = " + FQFN
+        "WHERE FQFN = ?", [FQFN]
     )
     conn.commit()
     return True
@@ -78,9 +115,10 @@ def updateSong(newData: dict):
     body = ''
     for key, val in newData.items():
         body += key + ' = :' + key + ',\n'
+    body = body[0:-2]
     fullSQL = (
         "UPDATE Songs SET\n"
-        + body[0:-2] + "\n"
+        + body + "\n"
         + "WHERE FQFN = :FQFN\n"
     )
 
