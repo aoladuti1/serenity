1e916b244db82961427ba212d9440efc8e068362
Antonio <ZJAC020@live.rhul.ac.uk>
2022-08-26 17:23:40 +0100

    removed useless import

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from tkinter import filedialog
 from config import *
 from aplayer import *
 import records
 import db
 from tkinter import *
+
+
+
+
 
 db.init()
 jukebox = Aplayer()
@@ -13,5 +16,6 @@ root = Tk()
 def on_closing():
     root.destroy()
     Aplayer.terminate()
+
 root.protocol("WM_DELETE_WINDOW", on_closing)
 root.mainloop()
